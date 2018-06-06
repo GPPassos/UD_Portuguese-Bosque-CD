e2ce2a5bef5682b1d50a8b6aafd83c4c01dccad9
Guilherme Paulino Passos <gpaulino@br.ibm.com>
2018-06-06 19:31:32 -0300

    Trying python language in order to use scripts later

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+### This is the script called by .travis.yml.
+### Deployment through Travis-CI for this repository should be
+### implemented here.
+
 bash ./scripts/join-documents.sh > automatic-results/corpus.conllu
