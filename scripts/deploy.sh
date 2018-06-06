00b06533e410cfca3845ebfb08b5a228af411a29
Guilherme Paulino Passos <gpaulino@br.ibm.com>
2018-06-06 19:37:02 -0300

    Trying validate.py

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,9 @@
 ### Deployment through Travis-CI for this repository should be
 ### implemented here.
 
+## Merge corpus in one file
 bash ./scripts/join-documents.sh > automatic-results/corpus.conllu
+
+## Run validate.py from UniversalDependencies/tools
+git clone https://github.com/UniversalDependencies/tools.git UniversalDependencies/tools
+python UniversalDependencies/tools/validate.py documents/CF0001.conllu > automatic-results/validate.log
