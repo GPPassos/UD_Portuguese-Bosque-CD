0a18258541e659240e698724a33de4e7c52c338a
Guilherme Paulino Passos <gpaulino@br.ibm.com>
2018-06-08 16:45:23 -0300

    Trying to produce stats

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,18 +3,24 @@
 ### This is the script called by .travis.yml.
 ### Deployment through Travis-CI for this repository should be
 ### implemented here.
+OUTPUT_DIR="./automatic-results"
 
 ## Merge corpus in one file
-bash ./scripts/join-documents.sh > automatic-results/corpus.conllu
+bash ./scripts/join-documents.sh > $OUTPUT_DIR/corpus.conllu
 
 ## Run validate.py from UniversalDependencies/tools
+echo -e "## Validation results: ##\n"
+
 git clone https://github.com/UniversalDependencies/tools.git UniversalDependencies/tools
 
-echo -e "## Validation results: ##\n"
-if cat documents/CF0001.conllu| python UniversalDependencies/tools/validate.py --lang pt 2> automatic-results/validate.log ; then
-    echo -e "\n  * \`UniversalDependencies/tools/validate.py\`: **SUCCESSFUL**" >> automatic-results/README.md ;
+if cat documents/CF0001.conllu| python UniversalDependencies/tools/validate.py --lang pt 2> $OUTPUT_DIR/validate.log ; then
+    echo -e "\n  * \`UniversalDependencies/tools/validate.py\`: **SUCCESSFUL**" >> $OUTPUT_DIR/README.md ;
 else
-    echo -e "\n  * \`UniversalDependencies/tools/validate.py\`: **FAILED** -- see \`validate.log\`" >> automatic-results/README.md ;
+    echo -e "\n  * \`UniversalDependencies/tools/validate.py\`: **FAILED** -- see \`validate.log\`" >> $OUTPUT_DIR/README.md ;
 fi
+
+## Run conllu-stats.pl in order to produce stats.xml file
+cat $OUTPUT_DIR/corpus.conllu | perl UniversalDependencies/tools/conllu-stats.pl > $OUTPUT_DIR/stats.xml
+
 
 exit 0
